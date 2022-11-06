3f8d90d723b700edf53133f52d19dc541ba297b5
Maksim Mishin <mihsin_ma29@mail.ru>
2022-11-06 15:24:16 +0300

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int eaters = 5; //сколько людей будут есть
+        int billet = 1985; // стоимость биллета
+        int milya = 20; // количества рублей для одной бонусной мили
+        int bonus = (billet/milya); // количество бонусных миль
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
 
-        System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
-        System.out.println((spices / eaters) + " ложек(ка) специй");
+        System.out.println(bonus +" бонусных миль");
+
     }
 }
