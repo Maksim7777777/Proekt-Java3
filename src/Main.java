fce171e378b23e8815911dea2f6324ee848a8488
Maksim Mishin <mihsin_ma29@mail.ru>
2022-11-07 13:13:58 +0300

    Commit 4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int billet = 1985; // стоимость биллета
-        int milya = 20; // количества рублей для одной бонусной мили
-        int bonus = (billet/milya); // количество бонусных миль
-
-
-        System.out.println(bonus +" бонусных миль");
+        int balans = 300; // деьги на счету у клиента
+        int popolnenieBalansa = 1550; // пополнение баланса
+        int prosent = (popolnenieBalansa / 100);
+        int itogSchet = (balans + popolnenieBalansa); // итоговый счет
+        if (popolnenieBalansa >= 1000) {
+             System.out.println(prosent + " бонусных рублей на счету"); // бонусных рублей на счету
+             System.out.println(balans + popolnenieBalansa + prosent + " рублей на счету"); // итоговый счес с %
+        } else {
+            System.out.println(itogSchet + " рублей на счету"); // итоговый счет без %
+        }
 
     }
 }
