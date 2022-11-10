cf22bb5fe8fb790808b1181af4ba70ab6f1ed427
Maksim Mishin <mihsin_ma29@mail.ru>
2022-11-10 16:20:26 +0300

    Commit 5

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int balans = 300; // деьги на счету у клиента
-        int popolnenieBalansa = 1550; // пополнение баланса
-        int prosent = (popolnenieBalansa / 100);
-        int itogSchet = (balans + popolnenieBalansa); // итоговый счет
-        if (popolnenieBalansa >= 1000) {
-             System.out.println(prosent + " бонусных рублей на счету"); // бонусных рублей на счету
-             System.out.println(balans + popolnenieBalansa + prosent + " рублей на счету"); // итоговый счес с %
+        int popolneniyeBalansa = 1550; //пополнение баланса.
+        int protsent = (popolneniyeBalansa / 100);
+        int itogSchot = (balans + popolneniyeBalansa); //итоговый счет
+        if (popolneniyeBalansa >= 1000) {
+            System.out.println(protsent + " бонусных рублей на счету"); // бонусных рублей на счету
+            System.out.println(balans + popolneniyeBalansa + protsent + " рублей на счету"); // итоговый счес с %
         } else {
-            System.out.println(itogSchet + " рублей на счету"); // итоговый счет без %
+            System.out.println(itogSchot + " рублей на счету"); // итоговый счет без %
         }
 
     }
